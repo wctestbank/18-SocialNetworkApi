5925094b6e82f1173b027f11fd6e2e7d8bfa4d72
wctestbank <wc.businessmail@gmail.com>
2022-06-01 13:17:18 -0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+// user schema
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    },
+    friends: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+
+const User = model('User', UserSchema);
+
+// export
+module.exports = User;
