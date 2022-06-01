ab2d212c3ffd2c570c1a1ec62d2632a4ca5ce277
wctestbank <wc.businessmail@gmail.com>
2022-06-01 15:00:15 -0400

    cleaned up files

--- models/Thought.js
+++ models/Thought.js
@@ -4,25 +4,33 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (dateInput) => dateFormat(dateInput)
-  },
-  username: {
-      type: String,
-      required: true
-  },
-  reactions: {
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (dateInput) => dateFormat(dateInput)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: {
 
-  }
-});
+    }
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
 const Thought = model('Thought', ThoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -25,6 +25,7 @@ const UserSchema = new Schema({
     {
         toJSON: {
             virtuals: true,
+            getters:true
         },
         id: false
     }
